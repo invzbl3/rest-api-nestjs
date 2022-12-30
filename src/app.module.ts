d5378c1b36c147c718a93c963887b4607ddbf21a
Maksym Rohachov <romax982@gmail.com>
2022-12-30 21:03:51 +0100

    Update code

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './rest-api-nestjs/src/user/user.module';
 import { UserController } from './rest-api-nestjs/src/user/controller/user.controller';
-import { UserService } from './rest-api-nestjs/src/user/service/user/user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [TypeOrmModule.forRoot(), UserModule],
   controllers: [AppController, UserController],
-  providers: [AppService, UserService, UserModule],
+  providers: [AppService],
 })
 export class AppModule {}
