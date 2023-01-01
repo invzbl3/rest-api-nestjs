afef28816bff43ded33ebbbf4da45503a559f45b
Maksym Rohachov <romax982@gmail.com>
2023-01-01 03:57:35 +0100

    Update the project logic

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { UserModule } from './rest-api-nestjs/src/user/user.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UserModule],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
